1531570a470253a76ba36cc237362d85a729fd86
Maroun Ayli <marounayle@gmail.com>
2020-10-18 16:26:10 +0300

    First heroku commit

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
+app.use(express.static('./dist/'));
+app.get('/*', function (req, res) {
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/black-dashboard-angular'));
+    res.sendFile('index.html', { root: 'dist' }
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/black-dashboard-angular/index.html'));
+    );
+
 });
-
-// Start the app by listening on the default Heroku port
-
-app.listen(process.env.PORT || 8080, () => console.log("listening"));
+app.listen(process.env.PORT || 8080);
+console.log(`Running on port ${process.env.PORT || 8080}​​`)
