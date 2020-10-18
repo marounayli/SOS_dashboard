0e26e58ae302624a1c52f672589f60931aea5f6f
Jean Khoury <jeankhoury_98@hotmail.com>
2020-10-18 12:58:37 +0300

    Initial commit

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { SidebarComponent } from "./sidebar/sidebar.component";
+
+@NgModule({
+  imports: [CommonModule, RouterModule, NgbModule],
+  declarations: [SidebarComponent],
+  exports: [SidebarComponent]
+})
+export class ComponentsModule {}
